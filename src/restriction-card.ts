e868abff21baa37d095a041dff0b0d5566071c68
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-02-16 03:28:11 +0300

    test

--- src/restriction-card.ts
+++ src/restriction-card.ts
@@ -212,8 +212,9 @@ class RestrictionCard extends LitElement implements LovelaceCard {
         });
 
         const conditionString = pin != (this._config.restrictions.pin.code as string);
-        const conditionArray = (this._config.restrictions.pin.code as string[]).includes(pin);
-        if ((!isMultiplePins && conditionString) || (isMultiplePins && !conditionArray)) {
+        const conditionArrayString = (this._config.restrictions.pin.code as string[]).includes(pin);
+        const conditionArrayNumber = (this._config.restrictions.pin.code as string[]).includes(Number(pin));
+        if ((!isMultiplePins && conditionString) || (isMultiplePins && !conditionArrayString && !conditionArrayNumber)) {
           lock.classList.add('invalid');
           this._delay = Boolean(this._config.restrictions.pin.retry_delay);
           if (this._config.restrictions.pin.max_retries) {
