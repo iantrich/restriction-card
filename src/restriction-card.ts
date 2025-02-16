404c6fa082bcfae7e99606623bd622e6efd666e0
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-02-16 04:46:05 +0300

    test

--- src/restriction-card.ts
+++ src/restriction-card.ts
@@ -216,7 +216,10 @@ class RestrictionCard extends LitElement implements LovelaceCard {
 
         let conditionArray = false;
         for (const pinElement of this._config.restrictions.pin.code as string[]) {
-          if ((pinElement as string) === pin) conditionArray = true;
+          if ((pinElement as string) === pin) {
+            conditionArray = true;
+            break;
+          }
         }
 
         if ((!isMultiplePins && conditionString) || (isMultiplePins && !conditionArray)) {
