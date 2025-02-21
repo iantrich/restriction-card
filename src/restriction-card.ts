70561349dc82bc0bf24a80416522ce684b9bba96
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2025-02-21 05:21:34 +0300

    dependencies

--- src/restriction-card.ts
+++ src/restriction-card.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { TemplateResult, customElement, LitElement, property, html, CSSResult, css, PropertyValues } from 'lit';
-import { classMap } from 'lit/directives/class-map';
-import { styleMap } from 'lit/directives/style-map.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
+import { TemplateResult, customElement, LitElement, property, html, CSSResult, css, PropertyValues } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+import { styleMap } from 'lit-html/directives/style-map.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { RestrictionCardConfig } from './types';
 import { HomeAssistant, LovelaceCard, computeCardSize, LovelaceCardConfig, evaluateFilter } from 'custom-card-helpers';
 import { CARD_VERSION } from './const';
